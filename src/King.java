be7e3e38e97d0340e953a58eae32e1357cb53b13
RoPeak <Ronanpeacock2004@gmail.com>
2024-07-10 12:06:40 +0100

    King logic

--- src/King.java
+++ src/King.java
@@ -1,3 +1,26 @@
 public class King extends Piece {
-    
+    public King(PieceColour colour, PiecePosition position) {
+        super(colour, position);
+    }
+
+    @Override
+    public boolean isValidMove(PiecePosition newPosition, Piece[][] board) {
+        // Calculate difference between new pos and current pos
+        int rowDiff = Math.abs(position.getRow() - newPosition.getRow());
+        int colDiff = Math.abs(position.getCol() - newPosition.getCol());
+
+        // Kings can only move one square in any direction
+        boolean isOneSquare = rowDiff <= 1 && colDiff <= 1 && !(rowDiff == 0 && colDiff == 0);
+        if (!isOneSquare) {
+            return false;
+        }
+
+        // Ensure that target square is empty or has a piece of the opposite colour
+        Piece targetPiece = board[newPosition.getRow()][newPosition.getCol()];
+        if (targetPiece == null) {
+            return true;
+        } else {
+            return targetPiece.getColour() != this.getColour();
+        }
+    }
 }
